6fe6120614a78497e7a15f072f51acf7ed98ea87
Pratapfalabella <ext_psomesh@falabella.cl>
2022-03-28 18:05:33 +0530

    [1][Nishit/Pratap] changes to  pick apiurl from env variables

--- src/Books/BookModel.js
+++ src/Books/BookModel.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default class BookModel {
     static fetchAll = async () => {
-        const response = await axios.get('http://localhost:8080/books', this.authHeaders());
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/books`, this.authHeaders());
         return response.data;
     }
 
--- src/Books/BookModel.test.js
+++ src/Books/BookModel.test.js
@@ -8,7 +8,7 @@ describe('BookModel', () => {
     it('should fetch all books from api',  async () => {
         axios.get = jest.fn().mockResolvedValue({data: booksFactory()});
         const books = await BookModel.fetchAll();
-        const url = "http://localhost:8080/books";
+        const url = `${process.env.REACT_APP_API_URL}/books`;
         const headers = {"auth": {"password": "Password@123", "username": "system-user@bookshop.com"}};
         expect(axios.get).toHaveBeenCalledWith(url, headers);
         expect(books).toHaveLength(2);
