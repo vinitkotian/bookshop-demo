30e6127b6caef0975c8ab3d37aa48b4bfa5218d0
pauljason <pauljason9848@gmail.com>
2022-03-29 16:20:54 +0530

    + Manjot | added material ui and image.

--- src/Books/BooksTable.js
+++ src/Books/BooksTable.js
@@ -1,25 +1,38 @@
 import React from "react";
+import { DataGrid } from '@mui/x-data-grid';
 
 export default function BooksTable(props) {
     const books = props.books;
-    return <table>
-        <thead>
-        <tr>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Price</th>
-        </tr>
-        </thead>
-        <tbody>
-        {            
-            books.map((book) => {
-                return <tr key={book.id}>
-                    <td>{book.name}</td>
-                    <td>{book.authorName}</td>
-                    <td> {book.price.amount}</td>
-                </tr>
-            })
-        }
-        </tbody>
-    </table>
+
+    const columns = [
+        { field: 'bookImageUrl', headerName: 'Cover', width: 200 , renderCell: (params) => <img src={params.value} width={50} height={50}/>},
+        { field: 'name', headerName: 'Name', width: 200 },
+        { field: 'authorName', headerName: 'Author', width: 200 },
+        { field: 'price', headerName: 'Price', width: 200 }
+    ];
+
+    function getRows(){
+        const rows = [];
+        books.map((book) => {
+            const bookTemp = new Object();
+            bookTemp.id = book.id;
+            bookTemp.bookImageUrl = book.bookImageUrl;
+            bookTemp.name = book.name;
+            bookTemp.authorName = book.authorName;
+            bookTemp.price = book.price.amount;
+            rows.push(bookTemp);
+        })
+        return rows;
+    }
+
+    return (
+        <div style={{ height:700, width: '100%' }}>
+            <DataGrid
+                rows={getRows()}
+                columns={columns}
+                pageSize={10}
+                rowsPerPageOptions={[10]}
+            />
+        </div>
+    );
 }
