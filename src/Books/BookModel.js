f1db06d928ca90b0a292f7805c9c0ab346165d50
Pratapfalabella <ext_psomesh@falabella.cl>
2022-03-29 10:33:41 +0530

    [1][Nishit/Pratap] changes to package runtive env of heroku to pick env variables

--- src/Books/BookModel.js
+++ src/Books/BookModel.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+const env = runtimeEnv();
 export default class BookModel {
     static fetchAll = async () => {
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/books`, this.authHeaders());
+        const response = await axios.get(`${env.REACT_APP_API_URL}/books`, this.authHeaders());
         return response.data;
     }
 
--- src/Books/BookModel.test.js
+++ src/Books/BookModel.test.js
@@ -1,14 +1,15 @@
 import BookModel from "./BookModel";
 import booksFactory from "./__factory__/books-factory";
 import axios from 'axios';
-
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+const env = runtimeEnv();
 jest.mock('axios');
 
 describe('BookModel', () => {
     it('should fetch all books from api',  async () => {
         axios.get = jest.fn().mockResolvedValue({data: booksFactory()});
         const books = await BookModel.fetchAll();
-        const url = `${process.env.REACT_APP_API_URL}/books`;
+        const url = `${env.REACT_APP_API_URL}/books`;
         const headers = {"auth": {"password": "Password@123", "username": "system-user@bookshop.com"}};
         expect(axios.get).toHaveBeenCalledWith(url, headers);
         expect(books).toHaveLength(2);
